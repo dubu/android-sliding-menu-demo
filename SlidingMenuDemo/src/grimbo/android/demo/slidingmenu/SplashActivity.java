d215edb23de68f6816ea5214df4449edf25acfac
dubuapt <kozazz@hanmail.net>
2013-02-22 07:50:38 +0900

    add resource

---
+++ SlidingMenuDemo/src/grimbo/android/demo/slidingmenu/SplashActivity.java
@@ -0,0 +1,26 @@
+package grimbo.android.demo.slidingmenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+public class SplashActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.splash);
+        
+        Handler handler = new Handler () {
+         @Override
+         public void handleMessage(Message msg) {
+          finish();
+         }
+        };
+        
+        handler.sendEmptyMessageDelayed(0, 1000);
+    }
+}
+ 
+
